b6cc91b34d7f2c0f7e59ba42c3769a6dbb3fabce
IshanMK <e18173@eng.pdn.ac.lk>
2022-06-30 17:26:08 +0530

    When the user tried to access unknown property email sent with unknwon content problem solved

--- mailSender.js
+++ mailSender.js
@@ -21,77 +21,80 @@ class MailSender {
   }
 
   // Mail sending Function
+
   async sendEmail() {
     // console.log(moment().format("HH:mm:ss"));
 
     //All the security_configs are in a seperate file for security purposes
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      port: process.env.HOSTPORT,
-      secure: false,
-      service: process.env.SERVICE,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.PASSWORD,
-      },
-    });
-
-    const mailConfigurations = {
-      // It should be a string of sender email
-      from: `Ishan Maduranga ${process.env.EMAIL_USERNAME}`,
-
-      // Comma Separated list of mails
-      bcc: this.emailList,
-
-      // reply-to field
-      replyTo: `do not reply to this email ${process.env.EMAIL_USERNAME}`,
-
-      // send the list as bcc or cc
-      // bcc: secure_configuration.EMAIL_LIST_BCC,
-      // cc: secure_configuration.EMAIL_LIST_CC,
-
-      // Subject of Email
-      subject: "Sending Email using Node.js",
-
-      // This would be the text of email body
-      text:
-        details[this.property] !== undefined
-          ? details[this.property]
-          : details.unknown,
-
-      attachments: [
-        {
-          filename: "test.txt",
-          content: "Hello, Mail For Testing",
+    // If the user tried to access unknown property this won't be executed
+    if (details[this.property] !== undefined) {
+      const transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: process.env.HOSTPORT,
+        secure: false,
+        service: process.env.SERVICE,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.PASSWORD,
         },
-      ],
-    };
-
-    await scheduleJob(new Date(this.regDate), () => {
-      // verify connection configuration
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Server is ready to take our messages");
-
-          if (mailConfigurations.bcc.length !== 0) {
-            //
-            console.log("Email list is not empty!");
-
-            // Send Mail to the given list of mails , if sending fails log it to the console
-            transporter.sendMail(mailConfigurations, (err, info) => {
-              if (err) throw err;
-              console.log("Email Sent Successfully");
-              console.log(info);
-            });
-          } else {
-            console.log("Email list empty");
-            return;
-          }
-        }
       });
-    });
+
+      const mailConfigurations = {
+        // It should be a string of sender email
+        from: `Ishan Maduranga ${process.env.EMAIL_USERNAME}`,
+
+        // Comma Separated list of mails
+        bcc: this.emailList,
+
+        // reply-to field
+        replyTo: `do not reply to this email ${process.env.EMAIL_USERNAME}`,
+
+        // send the list as bcc or cc
+        // bcc: secure_configuration.EMAIL_LIST_BCC,
+        // cc: secure_configuration.EMAIL_LIST_CC,
+
+        // Subject of Email
+        subject: "Sending Email using Node.js",
+
+        // This would be the text of email body
+        text: details[this.property],
+
+        attachments: [
+          {
+            filename: "test.txt",
+            content: "Hello, Mail For Testing",
+          },
+        ],
+      };
+
+      await scheduleJob(new Date(this.regDate), () => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Server is ready to take our messages");
+
+            if (mailConfigurations.bcc.length !== 0) {
+              //
+              console.log("Email list is not empty!");
+
+              // Send Mail to the given list of mails , if sending fails log it to the console
+              transporter.sendMail(mailConfigurations, (err, info) => {
+                if (err) throw err;
+                console.log("Email Sent Successfully");
+                console.log(info);
+              });
+            } else {
+              console.log("Email list empty");
+              return;
+            }
+          }
+        });
+      });
+    } else {
+      console.log(`Unknown Property Accessed!!`);
+    }
   }
 }
 
