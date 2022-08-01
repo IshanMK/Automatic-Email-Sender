cc0a0e2a7cf8b733d8ad6bd81a4a7ce6a9c11ad9
IshanMK <e18173@eng.pdn.ac.lk>
2022-08-01 09:38:50 +0530

    Updated the code for passing an object

--- mailSender.js
+++ mailSender.js
@@ -2,7 +2,7 @@
  * E / 18 / 173
  * Kasthuripitiya K.A.I.M.
  * Automated Mail Sender
- * 29/06/2022
+ * 01/08/2022
  */
 
 import moment from "moment";
@@ -12,92 +12,82 @@ import nodemailer from "nodemailer";
 import * as dotenv from "dotenv"; //Read the .env file
 dotenv.config({ path: ".env.auth" }); //Read the .env file
 
-class MailSender {
-  // Constructor to set the email list
-  constructor(emailList, regDate, sender, property) {
-    this.emailList = emailList;
-    this.regDate = regDate;
-    this.sender = sender;
-    this.property = property;
-  }
+// Mail sending Function
 
-  // Mail sending Function
+const sendEmail = async (options) => {
+  // console.log(moment().format("HH:mm:ss"));
 
-  async sendEmail() {
-    // console.log(moment().format("HH:mm:ss"));
+  //All the security_configs are in a seperate file for security purposes
+  // If the user tried to access unknown property this won't be executed
+  if (details[options.property] !== undefined) {
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.HOSTPORT,
+      secure: false,
+      service: process.env.SERVICE,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-    //All the security_configs are in a seperate file for security purposes
-    // If the user tried to access unknown property this won't be executed
-    if (details[this.property] !== undefined) {
-      const transporter = nodemailer.createTransport({
-        host: process.env.HOST,
-        port: process.env.HOSTPORT,
-        secure: false,
-        service: process.env.SERVICE,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.PASSWORD,
-        },
-      });
+    const mailConfigurations = {
+      // It should be a string of sender email
+      from: `${options.sender} ${process.env.EMAIL_USERNAME}`,
 
-      const mailConfigurations = {
-        // It should be a string of sender email
-        from: `${this.sender} ${process.env.EMAIL_USERNAME}`,
+      // Comma Separated list of mails
+      bcc: options.emailList,
 
-        // Comma Separated list of mails
-        bcc: this.emailList,
+      // reply-to field
+      replyTo: `do not reply to this email ${process.env.EMAIL_USERNAME}`,
 
-        // reply-to field
-        replyTo: `do not reply to this email ${process.env.EMAIL_USERNAME}`,
+      // send the list as bcc or cc
+      // bcc: secure_configuration.EMAIL_LIST_BCC,
+      // cc: secure_configuration.EMAIL_LIST_CC,
 
-        // send the list as bcc or cc
-        // bcc: secure_configuration.EMAIL_LIST_BCC,
-        // cc: secure_configuration.EMAIL_LIST_CC,
+      // Subject of Email
+      subject: details[options.property].subject,
 
-        // Subject of Email
-        subject: details[this.property].subject,
+      // This would be the text of email body
+      text: details[options.property].message,
+      // html: `<h2>${details[options.property].message}</h2>`,
 
-        // This would be the text of email body
-        // text: details[this.property],
-        html: `<h2>${details[this.property].message}</h2>`,
+      // attachments: [
+      //   {
+      //     filename: "test.txt",
+      //     content: "Hello, Mail For Testing",
+      //   },
+      // ],
+    };
 
-        attachments: [
-          {
-            filename: "test.txt",
-            content: "Hello, Mail For Testing",
-          },
-        ],
-      };
+    await scheduleJob(new Date(options.scheduledDate), () => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our messages");
 
-      await scheduleJob(new Date(this.regDate), () => {
-        // verify connection configuration
-        transporter.verify(function (error, success) {
-          if (error) {
-            console.log(error);
+          if (mailConfigurations.bcc.length !== 0) {
+            //
+            console.log("email list is not empty!");
+
+            // Send Mail to the given list of mails , if sending fails log it to the console
+            transporter.sendMail(mailConfigurations, (err, info) => {
+              if (err) throw err;
+              console.log("email Sent Successfully");
+              console.log(info);
+            });
           } else {
-            console.log("Server is ready to take our messages");
-
-            if (mailConfigurations.bcc.length !== 0) {
-              //
-              console.log("Email list is not empty!");
-
-              // Send Mail to the given list of mails , if sending fails log it to the console
-              transporter.sendMail(mailConfigurations, (err, info) => {
-                if (err) throw err;
-                console.log("Email Sent Successfully");
-                console.log(info);
-              });
-            } else {
-              console.log("Email list empty");
-              return;
-            }
+            console.log("empty mail list..!!");
+            return;
           }
-        });
+        }
       });
-    } else {
-      console.log(`Unknown Property Accessed!!`);
-    }
+    });
+  } else {
+    console.log(`Unknown Property Accessed!!`);
   }
-}
+};
 
-export { MailSender };
+export default sendEmail;
